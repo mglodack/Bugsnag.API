9f5c197c44840e78d5953411a17a8930f373c6c5
Mathew Glodack <glodack@gmail.com>
2017-07-09 21:12:50 -0700

    Create and Implement ErrorsClient

---
+++ Bugsnag.API/Clients/IErrorsClient.cs
@@ -0,0 +1,45 @@
+﻿using Bugsnag.API.Models;
+using Restivus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.API.Clients
+{
+    public interface IErrorsClient
+    {
+        Task<PaginatedResponse<IReadOnlyCollection<Error>>> ListErrorsAsync(string projectId);
+    }
+
+    public class ErrorsClient : IErrorsClient
+    {
+        public ErrorsClient(IRestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        readonly IRestClient _restClient;
+
+        public async Task<PaginatedResponse<IReadOnlyCollection<Error>>> ListErrorsAsync(string projectId)
+        {
+            return await _restClient
+                .SendAsync(
+                    method: HttpMethod.Get,
+                    relativePath: "",
+                    mutateRequestMessage: _ => { },
+                    deserializeResponseAsync: async response =>
+                    {
+                        // TODO: Improve these error messages.
+                        response.EnsureSuccessStatusCode();
+
+                        var json = await response.Content.ReadAsStringAsync();
+                        var errors = Jil.JSON.Deserialize<IReadOnlyCollection<Error>>(json);
+
+                        return new PaginatedResponse<IReadOnlyCollection<Error>>(result: errors);
+                    });
+        }
+    }
+}
---
+++ Bugsnag.API/Models/Error.cs
@@ -0,0 +1,33 @@
+﻿using Jil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.API.Models
+{
+    public class Error
+    {
+        [JilDirective("id")]
+        public string Id { get; set; }
+
+        [JilDirective("project_id")]
+        public string ProjectId { get; set; }
+
+        [JilDirective("error_class")]
+        public string ErrorClass { get; set; }
+
+        [JilDirective("message")]
+        public string Message { get; set; }
+
+        [JilDirective("context")]
+        public string Context { get; set; }
+
+        [JilDirective("first_seen")]
+        public DateTimeOffset FirstSeen { get; set; }
+
+        [JilDirective("last_seen")]
+        public DateTimeOffset LastSeen { get; set; }
+    }
+}
